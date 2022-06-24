02b964814b72b0d3cc63b0efac585496715e8ddb
jensenra <r.jensen@stud.unibas.ch>
2022-06-24 11:25:36 -0400

    Trying to rectify git errors

--- src/search/treesearch_space.cc
+++ src/search/treesearch_space.cc
@@ -67,13 +67,14 @@ void TreeSearchNode::open_initial(int h) {
 
 void TreeSearchNode::open(const TreeSearchNode &parent_node,
                       const OperatorProxy &parent_op,
-                      int adjusted_cost) {
+                      int adjusted_cost, int h) {
     assert(info.status == TreeSearchNodeInfo::NEW);
     info.status = TreeSearchNodeInfo::OPEN;
     info.g = parent_node.info.g + adjusted_cost;
     info.real_g = parent_node.info.real_g + parent_op.get_cost();
     info.parent_state_id = parent_node.get_state().get_id();
     info.creating_operator = OperatorID(parent_op.get_id());
+    info.best_h = h;
 }
 
 void TreeSearchNode::update_g(int g_diff){
@@ -166,7 +167,7 @@ void TreeSearchSpace::trace_path(const State &goal_state,
     assert(current_state.get_registry() == &state_registry);
     assert(path.empty());
     for (;;) {
-        //cout << "trace" << current_state.get_id() << endl;
+        cout << "trace" << current_state.get_id() << endl;
         const TreeSearchNodeInfo &info = search_node_infos[current_state];
         if (info.creating_operator == OperatorID::no_operator) {
             assert(info.parent_state_id == StateID::no_state);
